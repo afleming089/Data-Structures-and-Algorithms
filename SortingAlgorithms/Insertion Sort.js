098db7d20eba50b7a403f1a792fe27577144c73b
Anton Fleming <aflemingrocks089@gmail.com>
2023-05-08 11:35:25 -0500

    Trying to not use splice in order to make code more efficent

--- SortingAlgorithms/Insertion Sort.js
+++ SortingAlgorithms/Insertion Sort.js
@@ -1,12 +1,10 @@
 function insertionSort(arr) {
-  if (arr[1] < arr[0]) [arr[0], arr[1]] = [arr[1], arr[0]];
-  for (let i = 2; i <= arr.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (arr[i] < arr[j]) {
-        const elementToMove = arr[i];
-        arr.splice(i, 1);
-        arr.splice(j, 0, elementToMove);
-      }
+  // if (arr[1] < arr[0]) [arr[0], arr[1]] = [arr[1], arr[0]];
+  for (let i = 0; i <= arr.length; i++) {
+    let j = arr.length - 1;
+    while (arr[i] > arr[j]) {
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+      j--;
     }
   }
   return arr;
@@ -14,8 +12,8 @@ function insertionSort(arr) {
 
 console.log(insertionSort([23, 22, 66, 10, 19, 17])); // [10, 17, 19, 22, 23,  66]
 
-console.log(insertionSort([45, 6, 81, 2, 3, 46, 87, 69, 4]));
-// returns [2,3,4,6,45,46,69,81,87]
+// console.log(insertionSort([45, 6, 81, 2, 3, 46, 87, 69, 4]));
+// // returns [2,3,4,6,45,46,69,81,87]
 
-// nearly sorted array
-console.log(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])); // returns [1,2,3,4,5,6,7,8]
+// // nearly sorted array
+// console.log(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])); // returns [1,2,3,4,5,6,7,8]
