199dddf2adecadd3b23b59d10c706588939ef4c2
Anton Fleming <aflemingrocks089@gmail.com>
2023-05-10 11:29:39 -0500

    Added a loop to merge the items of slice together. Last item is dropped when array has a odd number of elements. Working on fixing that

--- SortingAlgorithms/Complex/MergeSort.js
+++ SortingAlgorithms/Complex/MergeSort.js
@@ -1,5 +1,13 @@
+// find which value was already returned and then just remove it from split
+
 // used to merge two sorted arrays
 function merge(arr1, arr2) {
+  console.log(arr1, arr2);
+
+  // if either are undefined just return the other. This is incase either 1 or 2 have no value.
+  if (arr1 === undefined) return arr2;
+  if (arr2 === undefined) return arr1;
+
   // declare mergeArr
   let mergeArr = [];
 
@@ -17,10 +25,9 @@ function merge(arr1, arr2) {
       j++;
     }
   }
-
   // for loop that starts at the current index of the array and pushes the rest of the array onto mergeArr.
   for (i; i < arr1.length; i++) {
-    mergeArr.push(arr2[i]);
+    mergeArr.push(arr1[i]);
   }
 
   for (j; j < arr2.length; j++) {
@@ -44,15 +51,17 @@ function mergeSort(arr) {
 
   // then plug into merge because single arrays are already sorted
 
-  console.log(split);
   for (let i = 0; i < split.length; i++) {
-    if (split[i - 1] !== undefined) {
-      mergedArr.push(merge(split[i - 1], split[i]));
-      // slice split each time so values are not repeated
-      split = split.slice(i);
-      console.log("split" + split);
-    }
+    console.log("i " + i);
+    mergedArr.push(merge(split[i], split[i + 1]));
+    console.log("---------------------");
+    // slice split each time so values are not repeated
+    split = split.slice(i + 1);
+    console.log("split");
+    console.log(split);
   }
+
+  mergedArr.push(merge(split[0], split[1]));
 
   // return the sorted array
   return mergedArr;
